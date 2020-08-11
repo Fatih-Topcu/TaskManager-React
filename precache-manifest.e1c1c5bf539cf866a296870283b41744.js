98089f7c920857543705e9dfc7500be6b84ad0fd
fatih <fatihtopcu97@gmail.com>
2020-08-11 15:47:45 +0300

    Updates

---
+++ precache-manifest.e1c1c5bf539cf866a296870283b41744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56a36e5b6cdfd9c0dd634fb853c2f9e",
+    "url": "/TaskManager-React/index.html"
+  },
+  {
+    "revision": "dfb4a366eb0e99e0340e",
+    "url": "/TaskManager-React/static/css/main.cbe8baf2.chunk.css"
+  },
+  {
+    "revision": "3f1dd82354ae356116a2",
+    "url": "/TaskManager-React/static/js/2.02cd562c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskManager-React/static/js/2.02cd562c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb4a366eb0e99e0340e",
+    "url": "/TaskManager-React/static/js/main.b35c4ac4.chunk.js"
+  },
+  {
+    "revision": "516bee1629491ecb2280",
+    "url": "/TaskManager-React/static/js/runtime-main.0992c9d7.js"
+  }
+]);
