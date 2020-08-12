071efb0dc846416ca2633b2147d63f26722d4046
fatih <fatihtopcu97@gmail.com>
2020-08-12 15:32:37 +0300

    Updates

---
+++ precache-manifest.7444b040707aab84a2ece864ca622d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4094ec8c78f627a10f311a5bc7ddb3da",
+    "url": "/TaskManager-React/index.html"
+  },
+  {
+    "revision": "3fd73beab865a47f79a1",
+    "url": "/TaskManager-React/static/css/main.eb95d92b.chunk.css"
+  },
+  {
+    "revision": "678c28551b6f9e8ef477",
+    "url": "/TaskManager-React/static/js/2.dd5e1574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskManager-React/static/js/2.dd5e1574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd73beab865a47f79a1",
+    "url": "/TaskManager-React/static/js/main.dcfb6c08.chunk.js"
+  },
+  {
+    "revision": "516bee1629491ecb2280",
+    "url": "/TaskManager-React/static/js/runtime-main.0992c9d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TaskManager-React/precache-manifest.c45dcc56f91899a0c12e83629ac848ac.js"
+  "/TaskManager-React/precache-manifest.7444b040707aab84a2ece864ca622d8a.js"
 );
 
 self.addEventListener('message', (event) => {
